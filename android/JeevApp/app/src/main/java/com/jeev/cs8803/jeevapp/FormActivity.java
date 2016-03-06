4bbad07af6b269ecb7ea92f33fd53d2d4f392a24
Sreshta Vijay <sreshta.v@gmail.com>
2016-03-05 19:40:52 -0500

    Connecting to Watson

--- android/JeevApp/app/src/main/java/com/jeev/cs8803/jeevapp/FormActivity.java
+++ android/JeevApp/app/src/main/java/com/jeev/cs8803/jeevapp/FormActivity.java
@@ -1,11 +1,15 @@
 package com.jeev.cs8803.jeevapp;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
@@ -13,6 +17,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 
 public class FormActivity extends AppCompatActivity {
 
@@ -27,51 +32,51 @@ public class FormActivity extends AppCompatActivity {
             getSupportActionBar().setTitle("Jeev");
         }
 
+
         Button mEmailSignInButton = (Button) findViewById(R.id.select_question);
         mEmailSignInButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View arg0) {
-                String question = "What is mint?";
-                try {
-                    URL url = null;
-                    String response = null;
-                    String parameters = "qu_free_question=Does mint have healing properties?";
-                    url = new URL("http://143.215.97.119/v1/user_question");
-
-                    //create the connection
-                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                    connection.setDoOutput(true);
-                    connection.setRequestProperty("Content-Type",
-                            "application/x-www-form-urlencoded");
-                    //set the request method to GET
-                    connection.setRequestMethod("GET");
-                    //get the output stream from the connection you created
-                    OutputStreamWriter request = new OutputStreamWriter(connection.getOutputStream());
-                    //write your data to the ouputstream
-                    request.write(parameters);
-                    request.flush();
-                    request.close();
-                    String line = "";
-                    //create your inputsream
-                    InputStreamReader isr = new InputStreamReader(
-                            connection.getInputStream());
-                    //read in the data from input stream, this can be done a variety of ways
-                    BufferedReader reader = new BufferedReader(isr);
-                    StringBuilder sb = new StringBuilder();
-                    while ((line = reader.readLine()) != null) {
-                        sb.append(line + "\n");
+                Log.d("Watson", "clicked");
+                EditText mEdit   = (EditText)findViewById(R.id.size_value);
+                String size = mEdit.getText().toString();
+                Spinner mSpinner = (Spinner)findViewById(R.id.size_units);
+                String sizeUnits = mSpinner.getSelectedItem().toString();
+                final String question = "";
+                Thread thread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        getWatsonAnswer(question);
                     }
-                    //get the string version of the response data
-                    response = sb.toString();
-                    Toast.makeText(getApplicationContext(), response, Toast.LENGTH_LONG).show();
-                    //do what you want with the data now
-                    //always remember to close your input and output streams
-                    isr.close();
-                    reader.close();
-                } catch (Exception e) {
-                    Log.e("HTTP GET:", e.toString());
-                }
+                });
+                thread.start();
             }
         });
     }
 
+    private String getWatsonAnswer (String question) {
+        String response = null;
+
+        try {
+            Log.d("Watson", "starting");
+            URL url = new URL("http://143.215.97.119/v1/user_question?qu_free_question="+ URLEncoder.encode(question,"UTF-8"));
+
+            //create the connection
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            Log.d("Watson" , connection.toString());
+            BufferedReader rd =  new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line = "";
+            StringBuilder result = new StringBuilder();
+            while ((line = rd.readLine()) != null) {
+                result.append(line + "\n");
+            }
+            Log.d("Watson", result.toString());
+            response = result.toString();
+            Toast.makeText(getApplicationContext(), response, Toast.LENGTH_LONG).show();
+            rd.close();
+        } catch (Exception e) {
+            Log.e("HTTP GET:", e.toString());
+        }
+        return response;
+    }
 }
