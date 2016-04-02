f7dad066a70793b5e9a36665051e8af74df2b249
Sreshta Vijay <sreshta.v@gmail.com>
2016-04-02 12:34:33 -0400

    Adding speech to text input

--- android/JeevApp/app/src/main/java/com/jeev/cs8803/jeevapp/TextAndAudioActivity.java
+++ android/JeevApp/app/src/main/java/com/jeev/cs8803/jeevapp/TextAndAudioActivity.java
@@ -1,26 +1,33 @@
 package com.jeev.cs8803.jeevapp;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class TextAndAudioActivity extends AppCompatActivity {
 
     private String response = "test";
+    private final int REQ_CODE_SPEECH_INPUT = 100;
 
-    private String encoder (String text) {
+    private String encoder(String text) {
         try {
             return URLEncoder.encode(text, "UTF-8").replace("+", "%20");
-        } catch (UnsupportedEncodingException e) { 
+        } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
         return null;
@@ -50,7 +57,7 @@ public class TextAndAudioActivity extends AppCompatActivity {
             public void onClick(View arg0) {
                 Log.d("Watson", "clicked");
                 EditText mEditText = (EditText) findViewById(R.id.question_value);
-                final String question = "qu_free_question="+encoder(mEditText.getText().toString());
+                final String question = "qu_free_question=" + encoder(mEditText.getText().toString());
                 //final String question = "qu_free_question="+mEditText.getText().toString();
                 Thread thread = new Thread(new Runnable() {
                     @Override
@@ -63,6 +70,49 @@ public class TextAndAudioActivity extends AppCompatActivity {
                 thread.start();
             }
         });
+
+        ImageButton mButtonAudioButton = (ImageButton) findViewById(R.id.audio);
+        mButtonAudioButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                promptSpeechInput();
+            }
+        });
     }
 
+    /**
+     * Showing google speech input dialog
+     */
+    private void promptSpeechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                getString(R.string.speech_prompt));
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(), getString(R.string.speech_not_supported), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * Receiving speech input
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    EditText mEditText = (EditText) findViewById(R.id.question_value);
+                    mEditText.setText(result.get(0));
+                }
+                break;
+            }
+
+        }
+    }
 }
